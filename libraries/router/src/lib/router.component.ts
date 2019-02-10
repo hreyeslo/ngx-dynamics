d3134c814a5e30ccca514c1cc224b428963c941a
Héctor Reyes López <hector.reyes.lopez.17@gmail.com>
2019-02-10 18:36:49 +0100

    Initial config and first library

---
+++ libraries/router/src/lib/router.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-dynamics-router',
+  template: `
+    <p>
+      router works!
+    </p>
+  `,
+  styles: []
+})
+export class RouterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ libraries/router/src/lib/router.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { RouterComponent } from './router.component';
+
+@NgModule({
+  declarations: [RouterComponent],
+  imports: [
+  ],
+  exports: [RouterComponent]
+})
+export class RouterModule { }
